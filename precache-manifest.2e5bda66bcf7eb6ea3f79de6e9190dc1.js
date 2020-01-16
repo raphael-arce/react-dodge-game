f3fb6841bc8ac79928bd4d71f903026d0ff80c37
Raphael Arce <raphael.arce@campus.tu-berlin.de>
2020-01-16 22:11:10 +0100

    Updates

---
+++ precache-manifest.2e5bda66bcf7eb6ea3f79de6e9190dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c07426366a4b0b67c6e7305b6a6337a",
+    "url": "/react-dodge-game/index.html"
+  },
+  {
+    "revision": "df0ba880fb2479b78278",
+    "url": "/react-dodge-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f3a5cdfe062b1142bf39",
+    "url": "/react-dodge-game/static/js/2.3dc63b86.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-dodge-game/static/js/2.3dc63b86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df0ba880fb2479b78278",
+    "url": "/react-dodge-game/static/js/main.59ea5e15.chunk.js"
+  },
+  {
+    "revision": "e9777a3905e70d89cc9c",
+    "url": "/react-dodge-game/static/js/runtime-main.575e9bc3.js"
+  }
+]);
